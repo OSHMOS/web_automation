940ba61b9c19467fcc002465452b2e055b94ec1f
OSHMOS <ojames9707@gmail.com>
2022-04-07 13:21:45 +0900

    코드잇 : 여러 웹사이트 한꺼번에 가져오기

--- main.py
+++ main.py
@@ -3,6 +3,13 @@
 '''
 import requests
 
-response = requests.get('https://google.com')
-print(response)
-print(response.text)
+rating_pages = []
+# https://workey.codeit.kr/ratings/index?year=2010&month=1%weekIndex={i}
+for i in range(5):
+    url = f'https://workey.codeit.kr/ratings/index?year=2010&month=1%weekIndex={i}'
+    response = requests.get(url)
+    rating_page = response.txt
+    rating_pages.append(rating_page)
+
+print(len(rating_pages))
+print(rating_pages[0])

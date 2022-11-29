4551f6f8802b75044dec08ec29a64da1f3420628
SEUNGHYUN OH <seunghyunoh@SEUNGHYUNui-MacBookAir.local>
2022-11-29 16:19:18 +0900

    11.29 select, select_one

--- main.py
+++ main.py
@@ -9,4 +9,17 @@ response = requests.get(url)
 rating_page = response.text
 
 soup = BeautifulSoup(rating_page, 'html.parser')
-print(soup.select('table'))
+
+# select => 리스트 형식으로 출력
+program_title_tags = soup.select('td.program')
+
+program_titles = []
+for tag in program_title_tags:
+    program_titles.append(tag.get_text())
+
+print(program_title_tags)
+
+print(program_titles)
+
+# select_one => 리스트 중 첫 번째 요소를 출력
+print(soup.select_one('td.program'))

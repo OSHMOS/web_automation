d15f08c42e9375bf7b6cce2d2e9d513388cb0cab
OSHMOS <87176797+OSHMOS@users.noreply.github.com>
2022-12-08 04:00:17 +0900

    12.8 time.sleep()

--- main.py
+++ main.py
@@ -2,17 +2,20 @@
 파이썬으로 서버에게 요청 보내고 응답 확인하기
 '''
 # Selenium 임포트
+import time
 from selenium import webdriver
 
 # 크롬 드라이버 생성
 driver = webdriver.Chrome()
-driver.implicitly_wait(3)
+driver.implicitly_wait(3)  # 최대 3초를 기다려준다. # 전체 코드에 계속 적용된다. # 상호작용에 영향 X
 
 # 사이트 접속하기
 driver.get('https://workey.codeit.kr/costagram/index')
+time.sleep(1)  # 길게 설정하는만큼 실행 시간도 늘어난다. # 넣기 좋은 위치는 웹 페이지가 변할 때이다.
 
 # 로그인 클릭
 driver.find_element_by_css_selector('.top-nav__login-link').click()
+time.sleep(1)
 
 # 아이디와 비밀번호 입력
 driver.find_element_by_css_selector(

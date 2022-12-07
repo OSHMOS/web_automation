2a6c5fa5031d0be38d80586fbcaf7a8bc6c34a8c
OSHMOS <87176797+OSHMOS@users.noreply.github.com>
2022-12-08 04:09:16 +0900

    12.8 explicitly wait

--- main.py
+++ main.py
@@ -4,27 +4,38 @@
 # Selenium 임포트
 import time
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 # 크롬 드라이버 생성
 driver = webdriver.Chrome()
-driver.implicitly_wait(3)  # 최대 3초를 기다려준다. # 전체 코드에 계속 적용된다. # 상호작용에 영향 X
+# driver.implicitly_wait(3) explicitly wait와 함께 쓰는 것 권장 X
 
 # 사이트 접속하기
 driver.get('https://workey.codeit.kr/costagram/index')
-time.sleep(1)  # 길게 설정하는만큼 실행 시간도 늘어난다. # 넣기 좋은 위치는 웹 페이지가 변할 때이다.
+time.sleep(3)  # 이 코드는 explicitly wait와 같이 써도 무방하다.
 
 # 로그인 클릭
-driver.find_element_by_css_selector('.top-nav__login-link').click()
-time.sleep(1)
+login_link = WebDriverWait(driver, 3).until(
+    EC.element_to_be_clickable((By.CSS_SELECTOR, '.top-nav__login-link')))
+login_link.click()
+
+# 공통된 WebDriverWait
+wait = WebDriverWait(driver, 3)
 
 # 아이디와 비밀번호 입력
-driver.find_element_by_css_selector(
-    '.login-container__login-input').send_keys('codeit')
-driver.find_element_by_css_selector(
-    '.login-container__password-input').send_keys('datascience')
+id_box = wait.until(EC.visibility_of_element_located(
+    (By.CSS_SELECTOR, '.login-container__login-input')))
+id_box.send_keys('codeit')
+pw_box = wait.until(EC.visibility_of_element_located(
+    (By.CSS_SELECTOR, '.login-container__password-input')))
+pw_box.send_keys('datascience')
 
 # 로그인 시도
-driver.find_element_by_css_selector('.login-container__login-button').click()
+login_button = wait.until(EC.element_to_be_clickable(
+    (By.CSS_SELECTOR, '.login-container__login-button')))
+login_button.click()
 
 # 로그아웃 시도
 # driver.find_element_by_css_selector('.top-nav__login-link').click()

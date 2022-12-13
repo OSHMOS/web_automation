73a4a4083bb03cef719538b5b955cbd16e6ec97e
OSHMOS <87176797+OSHMOS@users.noreply.github.com>
2022-12-13 19:31:32 +0900

    12.13 infinite scroll

--- main.py
+++ main.py
@@ -16,9 +16,21 @@ driver = webdriver.Chrome()
 driver.get('https://workey.codeit.kr/music')
 time.sleep(3)
 
-driver.execute_script('window.scrollTo(0, 200);')
+# 현재 scrollHeight 가져오기
+last_height = driver.execute_script('return document.body.scrollHeight')
 
-scroll_height = driver.execute_script('return document.body.scrollHeight')
-print(scroll_height)
+while True:
+    # scrollHeight 스크롤
+    driver.execute_script('window.scrollTo(0, document.body.scrollHeight);')
+
+    # 새로운 내용 로딩될 때까지 기다림
+    time.sleep(0.5)
+
+    # 새로운 내용 로딩 됐는지 확인
+    new_height = driver.execute_script('return document.body.scrollHeight')
+    if new_height == last_height:
+        break
+    last_height = new_height
+
 # scrap 작업 후에 브라우저 닫기 가능
 # driver.quit()
